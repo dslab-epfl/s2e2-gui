a4dc10781368c6825bbe7fdc5386569558d2eb50
Davide <davide@davide-Aspire-5552.(none)>
2017-02-24 08:26:56 +0100

    simpler way to linebreak

--- upload/models.py
+++ upload/models.py
@@ -1,9 +1,9 @@
 class S2EOutput():
 	def __init__(self, s2e_out_dir):
 		with open(s2e_out_dir + "warnings.txt", 'r') as destination:
-			self.warnings = destination.read().replace("\n", " <br /> ")
+			self.warnings = destination.read()
 		with open(s2e_out_dir + "messages.txt", 'r') as destination:
-			self.messages = destination.read().replace("\n", " <br /> ")
+			self.messages = destination.read()
 		with open(s2e_out_dir + "info.txt", 'r') as destination:
-			self.info = destination.read().replace("\n", " <br /> ")
+			self.info = destination.read()
 
