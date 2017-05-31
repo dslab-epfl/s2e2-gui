c58231907d40b279a557c5b2b5e41678018ba756
gohoron <didiodavide@net2000.ch>
2017-05-31 23:24:50 +0200

    cleanup and update README.md

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # s2e2-gui
 GUI for interacting with S2E2
 
+To use the GUI:
+ 
+1.  You have to edit the S2E_ENVIRONEMENT_FOLDER_PATH variable located inside the file s2e_web/S2E_settings 
+	and set it to your s2e_environement path (visit https://github.com/S2E/s2e-env for more info about s2e-env).
+2.	Run `python manage.py runserver`.
+3.	Visit "localhost:8000" to access the GUI.
+4. 	Enjoy.
 
 
 Plugin annotation:
--- s2e_web/S2E_settings.py
+++ s2e_web/S2E_settings.py
@@ -1,6 +1,6 @@
 import os
 
-# Needs to be the S2E environement path
+# Needs to an S2E environement path
 S2E_ENVIRONEMENT_FOLDER_PATH = "/home/davide/tmp/s2e/"
 
 
@@ -11,9 +11,3 @@ S2E_PROJECT_FOLDER_PATH = os.path.join(S2E_ENVIRONEMENT_FOLDER_PATH, 'projects')
 S2E_BINARY_FOLDER_PATH = os.path.join(S2E_ENVIRONEMENT_FOLDER_PATH, 'binary')
 
 EXECUTION_TRACE_PARSER_SCRIPT_PATH = os.path.join(os.getcwd(), "tools/execution_tracer/execution_trace_parser.py")
-#S2E_ENV_CONFIG_LUA_NAME = "s2e-config.lua"
-
-#S2E_BINARY_FILE_NAME = "tutorial_binary"
-
-#S2E_BINARY_PATH = os.path.join(S2E_PROJECT_FOLDER_PATH, S2E_PROJECT_NAME, S2E_BINARY_FILE_NAME)
-#S2E_CONFIG_LUA_PATH = os.path.join(S2E_PROJECT_FOLDER_PATH, S2E_PROJECT_NAME, S2E_ENV_CONFIG_LUA_NAME)
